1958058c89787f0a548f194e7a5b3db4ac0addc1
ka3vas <tyrnis@gmail.com>
2018-06-21 20:35:01 +0200

    handling events

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -4,6 +4,37 @@ import { getFunName } from '../helpers';
 // import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component {
+  // constructor () {
+  //   super();
+  //   // this is now equal to the instance of this component
+  //   this.goToStore = this.goToStore.bind(this);
+  // }
+
+  myInput = React.createRef();
+
+  // handleClick () {
+  //   alert('hey!');
+  // }
+
+  // goToStore (e) {
+  //   // 1. Stop form from submitting
+  //   e.preventDefault();
+  //   console.log("Going to store!");
+  //   // 2. Get the value of input
+  //   console.log(this.myInput);
+  //   // 3. Change the page to /store/inputValue
+  // }
+
+  // goToStore is now a property
+  goToStore = (e) => {
+    // 1. Stop form from submitting
+    e.preventDefault();
+    // 2. Get the value of input
+    const storeName = this.myInput.value.value;
+    // 3. Change the page to /store/inputValue
+    this.props.history.push(`/store/${storeName}`);
+  }
+
   render () {
     // return <p>I am the Store Picker!</p>
 
@@ -17,13 +48,19 @@ class StorePicker extends React.Component {
     //   </form> 
     // )
 
-    
     return (
       <React.Fragment>
         { /* JSX Comment */ }
-        <form action="" className="store-selector">
+        <form action="" className="store-selector" onSubmit={this.goToStore}>
          <h2>Please Enter a Store</h2>
-         <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+         {/* <button onClick={this.handleClick}>Click me!</button> */}
+         <input 
+          type="text"
+          required
+          placeholder="Store Name"
+          defaultValue={getFunName()}
+          ref={this.myInput}
+        />
          <button type="submit">Visit this Store</button>
         </form> 
       </React.Fragment>
